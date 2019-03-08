4612bb9cfcf4dc218e383fc842863f0f4eaab52a
EmmaKibore <emmakibore@gmail.com>
2019-03-08 08:55:43 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,5 @@
  $(document).ready(function(){
-   function........() {
-     var weight = $("input#weight").val();
-     var height = $("input#height").val();
-     var bmi ="";
 
-
-     if(weight && height) {
-       bmi = Math.round(10 * weight/ Math.pow(height/100, 2))/10
-       if (isNaN(bmi) ) bmi = "";
-     }
-     $("#bmiresult").text(bmi);
-     return bmi;
-   }
    $("form#bmi").bind("keydown", function (){
      setTimeout(function(){
        showbmi();
@@ -26,3 +14,17 @@
      return false;
    });
  });
+
+ function showbmi() {
+   var weight = $("input#weight").val();
+   var height = $("input#height").val();
+   var bmi ="";
+
+
+   if(weight && height) {
+     bmi = Math.round(10 * weight/ Math.pow(height/100, 2))/10
+     if (isNaN(bmi) ) bmi = "";
+   }
+   $("#bmiresult").text("your bmi is "+bmi);
+   return bmi;
+ }
